5fb7a3d82320b493f4e8506b677c421675630828
zpei100 <zhengqing.pei@gmail.com>
2019-03-04 13:45:37 -0500

    added init.scss, fonts, weights

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname, '../../dist')))
 
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => console.log('Server up on port 3000'));
